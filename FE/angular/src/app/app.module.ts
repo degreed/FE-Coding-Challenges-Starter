ea0f091403775e07002332e4588c50f596f4073d
Akash Wasson <akashwasson@gmail.com>
2023-11-08 10:43:35 +0530

    removed components decalartion from app.module.ts

--- FE/angular/src/app/app.module.ts
+++ FE/angular/src/app/app.module.ts
@@ -3,26 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MovieComponent } from './components/movie/movie.component';
-import { MoviesComponent } from './components/movies/movies.component';
-import { DecadesComponent } from './components/navigation/decades/decades.component';
-import { GoBackComponent } from './components/navigation/go-back/go-back.component';
-import { GoDetailsComponent } from './components/navigation/go-details/go-details.component';
-import { GoImdbComponent } from './components/navigation/go-imdb/go-imdb.component';
 import { NavigationService } from './components/navigation/navigation.service';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
-    MoviesComponent,
-    MovieComponent,
-    SidebarComponent,
-    GoBackComponent,
-    GoDetailsComponent,
-    GoImdbComponent,
-    AppComponent,
-    DecadesComponent
+    AppComponent
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [DataService, NavigationService],
