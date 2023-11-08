9f1e7e8268571a73cf95a2667c8e67e7fed615e9
Akash Wasson <akashwasson@gmail.com>
2023-11-08 11:24:42 +0530

    created constants file

---
+++ FE/angular/src/app/constants/constants.ts
@@ -0,0 +1,6 @@
+export class Constants {
+    static posterUrl = 'https://m.media-amazon.com/images/M/';
+    static replacePosterUrl = '/assets/images/';
+    static serviceUrl = 'https://www.omdbapi.com/?apikey=f59b2e4b&';
+}
+
