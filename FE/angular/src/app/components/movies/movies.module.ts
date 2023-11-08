8921a8171c15f9cd9329b295246ffcd79870fe83
Akash Wasson <akashwasson@gmail.com>
2023-11-08 13:39:35 +0530

    movie-row component implemented in movies.component.ts

--- FE/angular/src/app/components/movies/movies.module.ts
+++ FE/angular/src/app/components/movies/movies.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies.component';
 import { DecadesModule } from '../navigation/decades/decades.module';
-import { GoDetailsModule } from '../navigation/go-details/go-details.module';
+import { MovieRowModule } from '../movie-row/movie-row.module';
 
 
 @NgModule({
@@ -14,7 +14,7 @@ import { GoDetailsModule } from '../navigation/go-details/go-details.module';
   imports: [
     CommonModule,
     DecadesModule,
-    GoDetailsModule,
+    MovieRowModule,
     MoviesRoutingModule,
   ],
   exports: [
